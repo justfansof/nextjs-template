52de07032cef5e236d2333e9568fba9848bb9648
Just Fans Of <justfansof@gmail.com>
2023-10-04 01:07:55 -0400

    Revert "EP Branch Push Test for Netlify"
    This reverts commit aea2802f8afa45348676c44dbebe52c0354437c7.

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Eastern Petroleum</title>
+        <title>Next.js Toolbox</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="/dist/output.css" rel="stylesheet"></link>
       </Head>
